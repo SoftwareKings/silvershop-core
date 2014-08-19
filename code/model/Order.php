SoftwareKings/silvershop-core

commit 4e59d14d924f220bbc872360c765fda28bec52fe

Only show notes section if notes are written.

code/model/Order.php
@@ -145,13 +145,16 @@ class Order extends DataObject {
 		$fields = new FieldList(new TabSet('Root', new Tab('Main')));
 		$fs = "<div class=\"field\">";
 		$fe = "</div>";
-		$fields->addFieldsToTab('Root.Main', array(
+		$parts = array(
 			DropdownField::create("Status", _t("STATUS", "Status"), self::get_order_status_options()),
 			LiteralField::create('Customer', $fs.$this->renderWith("OrderAdmin_Customer").$fe),
 			LiteralField::create('Addresses', $fs.$this->renderWith("OrderAdmin_Addresses").$fe),
-			LiteralField::create('Content', $fs.$this->renderWith("OrderAdmin_Content").$fe),
-			LiteralField::create('Notes', $fs.$this->renderWith("OrderAdmin_Notes").$fe)
-		));
+			LiteralField::create('Content', $fs.$this->renderWith("OrderAdmin_Content").$fe)
+		);
+		if($this->Notes){
+			$parts[] = LiteralField::create('Notes', $fs.$this->renderWith("OrderAdmin_Notes").$fe);
+		}
+		$fields->addFieldsToTab('Root.Main', $parts);
 		$this->extend('updateCMSFields', $fields);
 		$payments = $fields->fieldByName("Root.Payments.Payments");
 		$fields->removeByName("Payments");

commit 2e158cd10d74bc52504672478f648d7a10222ad4

FIX: insert payments grid field after Content, instead of before Notes, because sometimes Notes field doesn't exist.
bug was introduced in 4e59d14d924f220bbc872360c765fda28bec52fe

code/model/Order.php
@@ -158,7 +158,7 @@ class Order extends DataObject {
 		$this->extend('updateCMSFields', $fields);
 		$payments = $fields->fieldByName("Root.Payments.Payments");
 		$fields->removeByName("Payments");
-		$fields->insertBefore($payments, "Notes");
+		$fields->insertAfter($payments, "Content");
 		$payments->addExtraClass("order-payments");
 
 		return $fields;